c8a5ab439f9526a48fbfe95c8cd1e8f62a12f141
Kaho-YAMAMOTO <rm0313kf@ed.ritsumei.ac.jp>
2026-04-25 05:03:32 +0900

    微修正

--- natsurobo_early_access/src/zakiomni.hpp
+++ natsurobo_early_access/src/zakiomni.hpp
@@ -15,12 +15,12 @@
 //　よく調整する定数集(For Mabuchi 775 motor))
 #define cpr 8192//1回転あたり8000カウントと仮定
 #define DEADZONE_L 0.02// スティックのデッドゾーン
-const double max_target_cps = 6.0; // 1秒あたりの最大回転数
+const double max_target_cps = 8.0; // 1秒あたりの最大回転数
 const double Kp  = 10.0;//P制御(必要に応じて調整)
-const double Ki = 0.0; // I制御（必要に応じて調整）
-const double Imax = 50.0; // I制御の蓄積の上限（必要に応じて調整）
+const double Ki = 0.5; // I制御（必要に応じて調整）
+const double Imax = 30.0; // I制御の蓄積の上限（必要に応じて調整）
 const double motor_limit = 80.0; // モーターの出力の上限（0~100で）
-const int delta_power_limit = 10;// 出力変化の上限
+const int delta_power_limit = 6;// 出力変化の上限
 const double enc_max = 32767.0; // エンコーダーの最大値
 
 using namespace std::chrono_literals;
